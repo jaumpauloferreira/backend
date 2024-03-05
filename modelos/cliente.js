46313c4fc33f65b5b9d6c8e7ab01a54bf988450d
João Paulo Ferreira da Silva <137740174+jaumpauloferreira@users.noreply.github.com>
2024-03-05 13:31:39 -0300

    atualização de eventos

--- modelos/cliente.js
+++ modelos/cliente.js
@@ -1,5 +1,5 @@
 
-import clienteDAO from "../percistencia/clienteDAO.js";
+import clienteDAO from "../percistencia/ClienteDAO.js";
 export default class Cliente {
     #codigo;
     #nome;
@@ -8,16 +8,20 @@ export default class Cliente {
     #rg;
     #cpf;
     #endereco;
+    #ingressos;
+    #valor;
 
-    constructor(codigo = 0, nome = "", email = "", telefone = "", rg = "", cpf = "", endereco = "") {
-        this.#codigo = codigo;
-        this.#nome = nome;
-        this.#email = email;
-        this.#telefone = telefone;
-        this.#rg = rg;
-        this.#cpf = cpf;
-        this.#endereco = endereco;
-    }
+   constructor(codigo = 0, nome = "", email = "", telefone = "", rg = "", cpf = "", endereco = "", ingressos = "", valor = "") {
+    this.#codigo = codigo;
+    this.#nome = nome;
+    this.#email = email;
+    this.#telefone = telefone;
+    this.#rg = rg;
+    this.#cpf = cpf;
+    this.#endereco = endereco;
+    this.#ingressos = ingressos;
+    this.#valor = valor;
+}
 
     get codigo() {
         return this.#codigo;
@@ -75,6 +79,22 @@ export default class Cliente {
         this.#endereco = novoEndereco;
     }
 
+        get ingressos() {
+        return this.#ingressos;
+    }
+
+    set ingressos(novosIngressos) {
+        this.#ingressos = novosIngressos;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    set valor(novoValor) {
+        this.#valor = novoValor;
+    }
+
     //como armazenar os clientes no banco de dados?
 
     async gravar(){
@@ -82,13 +102,40 @@ export default class Cliente {
         await dao.gravar(this);
     }
 
-    atualizar(){}
+    async atualizar(){
+    const dao = new EventoDAO();
+    await dao.atualizar(this); 
+}
     
-    excluir(){}
+    async excluir(){
+    const dao = new EventoDAO();
+    await dao.excluir(this);
+}
     
-    consultar(termoDePesquisa){}
+    async consultar(termoDePesquisa){
+    const dao = new EventoDAO();
+    return await dao.consultar(termoDePesquisa);
+}
+
+//override do método toString da classe pai Object
+    toString(){
+        return `Cliente código: ${this.#codigo} -  nome: ${this.#nome}`;
+    }
 
 
+toJSON(){
+        return {
+            "codigo": this.#codigo,
+            "nome": this.#nome,
+            "email": this.#email,
+            "telefone": this.#telefone,
+            "rg": this.#rg,
+            "cpf": this.#cpf,
+            "endereco": this.#endereco,
+            "ingresso": this.#ingressos,
+            "valor": this.#valor
+            }
+    }    
 }
 
 
--- percistencia/clienteDAO.js
+++ percistencia/clienteDAO.js
@@ -1,95 +1,84 @@
 import conectar from "./conexao.js"; //não esquecer de colocar a extensão .js no final
-import cliente from "../modelos/cliente.js";
-export default class clienteDAO{
-
-   async gravar(cliente){
-        if (cliente instanceof cliente){
-            const conexao = await conectar();
-            const sql = `INSERT INTO cliente (id, codigo, nome, email, telefone, rg, cpf, endereco)
+import Cliente from "../modelos/Cliente.js";
+export default class ClienteDAO {
+  async gravar(Cliente) {
+    if (cliente instanceof Cliente) {
+      const conexao = await conectar();
+      const sql = `INSERT INTO cliente (id, codigo, nome, email, telefone, rg, cpf, endereco)
             values (?, ?, ?, ?, ?, ?, ?, ?)`;
-        const parametros = [
-            cliente.id,
-            cliente.codigo,
-            cliente.nome,
-            cliente.email,
-            cliente.telefone,
-            cliente.rg,
-            cliente.cpf,
-            cliente.endereco
-        ];
-        const [resultados, campos] = await conexao.execute(sql, parametros);
-        //funcionalidade oferecida pela biblioteca mysql2
-        cliente.codigo = resultados.insertId; //recupero o id gerado pelo banco de dados
-            
-        }
-    
+      const parametros = [
+        cliente.id,
+        cliente.codigo,
+        cliente.nome,
+        cliente.email,
+        cliente.telefone,
+        cliente.rg,
+        cliente.cpf,
+        cliente.endereco,
+      ];
+      const [resultados, campos] = await conexao.execute(sql, parametros);
+      //funcionalidade oferecida pela biblioteca mysql2
+      cliente.codigo = resultados.insertId; //recupero o id gerado pelo banco de dados
     }
+  }
 
-    async atualizar(cliente){
-        if (cliente instanceof cliente){
-            const conexao = await conectar();
-            const sql = `UPDATE cliente SET id = ?, codigo = ?, nome = ?. email = ?, telefone = ?, rg = ?,
+  async atualizar(cliente) {
+    if (cliente instanceof cliente) {
+      const conexao = await conectar();
+      const sql = `UPDATE cliente SET id = ?, codigo = ?, nome = ?. email = ?, telefone = ?, rg = ?,
                          cpf = ?, endereco = ? HERE codigo = ?`;
-            const parametros = [
-                cliente.id,
-                cliente.codigo,
-                cliente.nome,
-                cliente.email,
-                cliente.telefone,
-                cliente.rg,
-                cliente.cpf,
-                cliente.endereco   
-            ];
+      const parametros = [
+        cliente.id,
+        cliente.codigo,
+        cliente.nome,
+        cliente.email,
+        cliente.telefone,
+        cliente.rg,
+        cliente.cpf,
+        cliente.endereco,
+      ];
 
-            await conexao.execute(sql,parametros);
-            
-        }
+      await conexao.execute(sql, parametros);
+    }
+  }
+
+  async excluir(cliente) {
+    if (cliente instanceof cliente) {
+      const conexao = await conectar();
+      const sql = `DELETE FROM cliente WHERE codigo = ?`;
+      const parametros = [cliente.codigo];
+      await conexao.execute(sql, parametros);
+    }
+  }
+
+  async consultar(termoDePesquisa) {
+    if (termoDePesquisa === undefined) {
+      termoDePesquisa = "";
+    }
+    let sql = "";
+    if (isNaN(termoDePesquisa)) {
+      //termo de pesquisa não é um número
+      sql = `SELECT = FROM cliente WHERE nome LIKE '%?%`;
+    } else {
+      sql = `SELECT = FROM cliente WHERE codigo = ?`;
     }
 
-
-    async excluir(cliente){
-        if (cliente instanceof cliente){
-            const conexao = await conectar();
-            const sql = `DELETE FROM cliente WHERE codigo = ?`;
-            const parametros = [
-                cliente.codigo
-            ]
-            await conexao.execute(sql,parametros);
-        }
+    const conexao = await conectar();
+    const [registros] = await conexao.execute(sql, [termoDePesquisa]);
+    let listaCliente = [];
+    for (const registro of registros) {
+      const cliente = new cliente(
+        registro.id,
+        registro.codigo,
+        registro.nome,
+        registro.email,
+        registro.telefone,
+        registro.rg,
+        registro.cpf,
+        registro.endereco
+      );
+      listaCliente.push(cliente);
     }
-
-
-
-    async consultar(termoDePesquisa){
-        if (termoDePesquisa === undefined){
-            termoDePesquisa = "";
-        }
-        let sql="";
-        if (isNaN(termoDePesquisa)) { //termo de pesquisa não é um número
-            sql = `SELECT = FROM cliente WHERE nome LIKE '%?%`;
-        }   
-        else{
-            sql = `SELECT = FROM cliente WHERE codigo = ?`;
-        }
-
-        const conexao = await conectar();
-        const [registros] = await conexao.execute(sql,[termoDePesquisa]);
-        let listaCliente = [];
-        for (const registro of registros){
-            const cliente = new cliente(
-                registro.id,
-                registro.codigo,
-                registro.nome,
-                registro.email,
-                registro.telefone,
-                registro.rg,
-                registro.cpf,
-                registro.endereco
-            );
-            listaCliente.push(cliente);
-        }
-        return listaCliente;
-    }
-
-
+    return listaCliente;
+  }
 }
